4a79c6f31c723465c32bcbb13ef88b9b28117868
Sam Gauck <21samuelg@abingtonfriends.net>
2017-11-13 15:03:47 -0500

    Created City, added Scanner to Main
    Created City, added Scanner to Main. No actual functionality

---
+++ src/com/samgauck/City.java
@@ -0,0 +1,17 @@
+package com.samgauck;
+
+public class City {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public City(String name) {
+        this.name = name;
+    }
+}
---
+++ src/com/samgauck/Main.java
@@ -0,0 +1,12 @@
+package com.samgauck;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner s = new Scanner(System.out);
+    public static void main(String[] args) {
+	// TODO: Auto-generated method stub
+
+    }
+}
