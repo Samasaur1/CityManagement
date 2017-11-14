4a956989e3552cdbec32563976f81bc71d3fe119
Sam Gauck <21samuelg@abingtonfriends.net>
2017-11-14 11:01:29 -0500

    Adds comments
    Not done

--- src/com/samgauck/Main.java
+++ src/com/samgauck/Main.java
@@ -21,11 +21,11 @@ public class Main {
         System.out.println(city.listCitizens());
     }
     private static void start() {
-        date = new SimpleDate(1,1,2020);
+        date = new SimpleDate(1,1,2020); //Sets game date to 1/1/2020
         System.out.println("Sam Gauck Presents:");
         millis = System.currentTimeMillis();
         while (true){
-            if (!(System.currentTimeMillis() - millis < 1000)) break;
+            if (!(System.currentTimeMillis() - millis < 1000)) break; //one second wait
         }
         for (int i = 0; i < 50; i++){
             System.out.println();
@@ -34,15 +34,20 @@ public class Main {
         System.out.println("Version 1.0");
         millis = System.currentTimeMillis();
         while (true){
-            if (!(System.currentTimeMillis() - millis < 1000)) break;
+            if (!(System.currentTimeMillis() - millis < 1000)) break; //one second wait
         }
         for (int i = 0; i < 50; i++){
             System.out.println();
         }
         System.out.print("Please enter a name for your city: ");
         input = s.next();
-        city = new City(input);
+        city = new City(input); //creates new city with your name
         System.out.println("Would you like to name your citizens? (Y/N)");
         input = s.nextLine();
+        if (input.equalsIgnoreCase("y")) {
+            //TODO: Name citizens
+        }else {
+            //TODO: random name citizens
+        }
     }
 }
