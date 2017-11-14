ef4b3dd50abe60bd43535fca88e038e2b26b3973
Samasaur1 <30577766+Samasaur1@users.noreply.github.com>
2017-11-14 11:21:00 -0500

    Adds comments

--- src/com/samgauck/City.java
+++ src/com/samgauck/City.java
@@ -3,9 +3,9 @@ package com.samgauck;
 import java.util.ArrayList;
 
 public class City {
-    private String name;
-    private ArrayList<Person> citizens = new ArrayList<Person>();
-    public void newPerson() {
+    private String name; //name of the city
+    private ArrayList<Person> citizens = new ArrayList<Person>(); //Holds all people in this city
+    public void newPerson() { //Creates and adds a new person
         Person p = new Person();
         citizens.add(p);
     }
@@ -18,18 +18,18 @@ public class City {
         this.name = name;
     }
 
-    public City(String name) {
+    public City(String name) { //creates a city named name
         this.name = name;
         newPerson();
-        newPerson();
-        citizens.forEach(person -> person.setAge(20));
+        newPerson(); //creates 2 new citizens
+        citizens.forEach(person -> person.setAge(20)); //sets the citizens to a working age
     }
 
     @Override
     public String toString() {
         return "A city named " + this.name;
     }
-    public String listCitizens() {
+    public String listCitizens() { //builds a string that is a multiple line list of citizens
         StringBuilder output = new StringBuilder();
         for (Person citizen : citizens) {
             output.append(citizen).append("\n");
