19c8e76ef4335f94d11f95dff63a736bb0aee9e4
Sam Gauck <21samuelg@abingtonfriends.net>
2017-11-15 17:56:09 -0500

    Adds WORKING toString() that outputs a correctly capitalized string
    YAHOOOO! YOU'RE ALL CLEAR, KID! NOW LET'S BLOW THIS THING AND GO HOME!

--- src/com/samgauck/Profession.java
+++ src/com/samgauck/Profession.java
@@ -8,6 +8,6 @@ public enum Profession {
 
     @Override
     public String toString() {
-        return super.toString();
+        return super.toString().substring(0, 1).toUpperCase() + super.toString().substring(1).toLowerCase();
     }
 }
--- src/com/samgauck/Sex.java
+++ src/com/samgauck/Sex.java
@@ -1,5 +1,10 @@
 package com.samgauck;
 
 public enum Sex {
-    MALE, FEMALE
+    MALE, FEMALE;
+
+    @Override
+    public String toString() {
+        return super.toString().substring(0, 1).toUpperCase() + super.toString().substring(1).toLowerCase();
+    }
 }
