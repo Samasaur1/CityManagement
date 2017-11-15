6e7d831ccde6fe2e19ed0579e785cd446511006a
Sam Gauck <21samuelg@abingtonfriends.net>
2017-11-15 18:25:05 -0500

    Adds (incomplete) Resources class to manage resources

---
+++ src/com/samgauck/Resources.java
@@ -0,0 +1,28 @@
+package com.samgauck;
+
+public class Resources {
+    private int money;
+    public void setMoney(int money) {
+        this.money = money;
+    }
+    public int getMoney() {
+        return money;
+    }
+
+    private int food;
+    public int getFood() {
+        return food;
+    }
+    public void setFood(int food) {
+        this.food = food;
+    }
+
+    public Resources() {
+        this.setMoney(0);
+        this.setFood(0);
+    }
+    public Resources(int money, int food) {
+        this.setMoney(money);
+        this.setFood(food);
+    }
+}
