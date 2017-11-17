24b40b45975abb5bceda5b8888153242f5b2252e
Samasaur1 <30577766+Samasaur1@users.noreply.github.com>
2017-11-17 17:41:02 -0500

    Adds average to Utilities

--- src/com/samgauck/Utilities.java
+++ src/com/samgauck/Utilities.java
@@ -5,4 +5,11 @@ public class Utilities {
     public static String capitailze(String original) {
         return original.substring(0, 1).toUpperCase() + original.substring(1).toLowerCase();
     }
+    public static double average(int[] values) {
+        int total = 0;
+        for (i : values) {
+            total += i;
+        }
+        return total/values.size();
+    }
 }
