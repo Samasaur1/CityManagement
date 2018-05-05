9ff0b936010043bfa407692caea466708f3570dd
Sam Gauck <21samuelg@abingtonfriends.net>
2018-05-05 13:33:49 -0400

    Add javadocs

--- src/main/java/com/samgauck/CityManagement/Transport.java
+++ src/main/java/com/samgauck/CityManagement/Transport.java
@@ -8,21 +8,43 @@ import java.util.ArrayList;
  * @author Samasaur
  */
 public class Transport {
+    /**
+     * The resources in transport.
+     */
     private Resources resources;
+    /**
+     * The people in transport.
+     */
     private ArrayList<Person> people;
 
+    /**
+     * Get a list of people being transported.
+     * @return The list of people in transit.
+     */
     public ArrayList<Person> getPeople() {
         return people;
     }
 
+    /**
+     * Get the resources being transported.
+     * @return The resources in transit.
+     */
     public Resources getResources() {
         return resources;
     }
 
+    /**
+     * Creates a new Transport with no resources or people.
+     */
     public Transport() {
         resources = new Resources();
         people = new ArrayList<>();
     }
+
+    /**
+     * Add a person to the transport.
+     * @param person The person to add.
+     */
     public void addPerson(Person person) {
         people.add(person);
     }
