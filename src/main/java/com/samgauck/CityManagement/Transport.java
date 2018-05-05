543eff1e23a53f818cb76631612700e48e3166b6
Sam Gauck <21samuelg@abingtonfriends.net>
2018-05-05 08:43:44 -0400

    Add equals implementation and getter methods

--- src/main/java/com/samgauck/CityManagement/Transport.java
+++ src/main/java/com/samgauck/CityManagement/Transport.java
@@ -8,13 +8,38 @@ import java.util.ArrayList;
  * @author Samasaur
  */
 public class Transport {
-    public Resources resources;
-    public ArrayList<Person> people;
+    private Resources resources;
+    private ArrayList<Person> people;
+
+    public ArrayList<Person> getPeople() {
+        return people;
+    }
+
+    public Resources getResources() {
+        return resources;
+    }
+
     public Transport() {
         resources = new Resources();
         people = new ArrayList<>();
     }
     public void addPerson(Person person) {
         people.add(person);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Transport other = (Transport) obj;
+        if (!(this.getPeople().equals(other.getPeople()))) return false;
+        return this.getResources().equals(other.getResources());
     }
 }
